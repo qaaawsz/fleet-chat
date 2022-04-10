68a138d09cb7976df633cfa12d0a3a2663f4bd6a
Anton <qaaawzs@gmail.com>
2022-04-10 23:46:31 +0300

    Server moved to heroku

--- client/src/components/Auth.tsx
+++ client/src/components/Auth.tsx
@@ -34,7 +34,7 @@ const Auth = () => {
         e.preventDefault()
         const {fullName, username, password, phoneNumber, avatarURL} = form
 
-        const URL = 'http://localhost:5000/auth'
+        const URL = 'https://fleetchat-qaaawzs.herokuapp.com/auth'
 
         const {data: {token, userId, hashedPassword}} = await axios.post(`${URL}/${isSignUp ? 'signup' : 'login'}`, {
             username, password, fullName: form.fullName, phoneNumber, avatarURL,
